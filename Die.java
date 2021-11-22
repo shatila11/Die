7eaa8ab7d61994e7e28198eca3677e9ed298f11e
Student: Tarik Tarik <94848828+shatila11@users.noreply.github.com>
2021-11-22 14:41:01 +0000

    Made die

---
+++ Die.java
@@ -0,0 +1,31 @@
+public class Die
+{
+  private int value;
+  private int numSides;
+  
+
+  public Die(int v, int n)
+  {
+
+  }
+
+
+  public int getValue()
+  {
+    return value;
+  }
+  
+  public int getNumSides()
+  {
+    return numSides;
+  }
+
+    public void  setValue(int v)
+  {
+    value = v;
+  }
+    public void  setNumSides(int ns)
+  {
+    numSides = ns;
+  }
+}
